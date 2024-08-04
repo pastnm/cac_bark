7f24869f4649efb04eef85565319f1c8e01061b6
GitHub Action <action@github.com>
2024-08-04 12:40:28 +0000

    save caches

--- Library/Caches/com.nan.cac/686449807_us.js
+++ Library/Caches/com.nan.cac/686449807_us.js
@@ -5,5 +5,5 @@
   "name": "Telegram Messenger",
   "version": "10.15.1",
   "price": "Free",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/fa/6c/a3/fa6ca36c-0e7e-b6ca-805f-9c9b473f5893/AppIconLLC-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/94/f7/41/94f7412e-f5a8-6c93-647f-74b2e1661a5b/AppIconLLC-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
 }
