a4c388f0be3ee3f58585c6eee033c5c2f531c45e
GitHub Action <action@github.com>
2024-08-27 22:27:00 +0000

    save caches

--- Library/Caches/com.nan.cac/1467190251_cn.js
+++ Library/Caches/com.nan.cac/1467190251_cn.js
@@ -3,7 +3,7 @@
   "bundle": "com.miHoYo.Yuanshen",
   "id": 1467190251,
   "name": "原神",
-  "version": "4.8.0",
+  "version": "5.0.0",
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/10/20/65/102065c0-6861-5ee9-b511-6e075ee0a9f5/AppIcon-1x_U007epad-0-0-85-220-0.png/100x100bb.jpg"
 }
