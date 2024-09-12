03b529e1464f1f5a2f96f523ee63e3a25870bab2
GitHub Action <action@github.com>
2024-09-12 14:47:11 +0000

    save caches

--- Library/Caches/com.nan.cac/1330863325_cn.js
+++ Library/Caches/com.nan.cac/1330863325_cn.js
@@ -5,5 +5,5 @@
   "name": "第五人格",
   "version": "24.145.669",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/2b/7f/9a/2b7f9ac3-69af-f5a8-2d3b-c0e16547f0a7/AppIcon-0-0-1x_U007emarketing-0-7-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7e/71/00/7e71003d-7923-dd6b-de86-a6c7988a01c8/AppIcon-0-0-1x_U007emarketing-0-10-85-220.png/100x100bb.jpg"
 }
