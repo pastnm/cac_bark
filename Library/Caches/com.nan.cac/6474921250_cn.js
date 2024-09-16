e9c3e153ad9c39d4765d3161cda2eab22be77726
GitHub Action <action@github.com>
2024-09-16 03:14:35 +0000

    save caches

---
+++ Library/Caches/com.nan.cac/6474921250_cn.js
@@ -0,0 +1,9 @@
+{
+  "status": 0,
+  "bundle": "com.jump.ShellBoxKit",
+  "id": 6474921250,
+  "name": "CareServer",
+  "version": 1.21,
+  "price": "免费",
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/31/7c/8b/317c8b8f-aca4-2472-7a80-d97b40e40616/AppIcon-0-0-1x_U007emarketing-0-85-220.png/100x100bb.jpg"
+}
