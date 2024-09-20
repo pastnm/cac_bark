68110e8af06d8f16bfc8a6912815a1e16e6d6e68
GitHub Action <action@github.com>
2024-09-20 04:49:29 +0000

    save caches

--- Library/Caches/com.nan.cac/414478124_cn.js
+++ Library/Caches/com.nan.cac/414478124_cn.js
@@ -3,7 +3,7 @@
   "bundle": "com.tencent.xin",
   "id": 414478124,
   "name": "微信",
-  "version": "8.0.51",
+  "version": "8.0.52",
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/38/9f/c7/389fc72c-cf34-b914-9549-90e10d928173/AppIcon-0-0-1x_U007emarketing-0-4-0-0-85-220.png/100x100bb.jpg"
 }
