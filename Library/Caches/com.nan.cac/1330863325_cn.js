f6b8c0d8df9f285661262d628ade5ded4950ae33
GitHub Action <action@github.com>
2024-10-17 06:38:02 +0000

    save caches

--- Library/Caches/com.nan.cac/1330863325_cn.js
+++ Library/Caches/com.nan.cac/1330863325_cn.js
@@ -3,7 +3,7 @@
   "bundle": "com.netease.id5",
   "id": 1330863325,
   "name": "第五人格",
-  "version": "24.145.671",
+  "version": "24.286.1185",
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/d5/53/a5/d553a5cc-7a91-0ecd-02bc-1c4956cd8c01/AppIcon-0-0-1x_U007emarketing-0-7-0-85-220.png/100x100bb.jpg"
 }
