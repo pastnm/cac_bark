7b36b084cb9a2b39bc070c146ef513205bfccb27
GitHub Action <action@github.com>
2024-10-30 23:28:38 +0000

    save caches

--- Library/Caches/com.nan.cac/414603431_cn.js
+++ Library/Caches/com.nan.cac/414603431_cn.js
@@ -5,5 +5,5 @@
   "name": "QQ音乐 - 听我想听",
   "version": "13.11.0",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/de/11/08/de11084b-4447-e4b8-2cf1-5c41ce3aa32e/AppIcon-0-1x_U007ephone-0-0-0-1-0-0-sRGB-85-220-0.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/46/3c/02/463c02e9-b3a0-7ebd-0adc-b9a976f9db75/AppIcon-0-1x_U007ephone-0-0-0-1-0-0-sRGB-85-220-0.png/100x100bb.jpg"
 }
