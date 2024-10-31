063a70010d129f95c8133507ffee7e7c6a5f2c65
GitHub Action <action@github.com>
2024-10-31 03:00:26 +0000

    save caches

--- Library/Caches/com.nan.cac/1330863325_cn.js
+++ Library/Caches/com.nan.cac/1330863325_cn.js
@@ -3,7 +3,7 @@
   "bundle": "com.netease.id5",
   "id": 1330863325,
   "name": "第五人格",
-  "version": "24.286.1185",
+  "version": "24.301.1014",
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/c5/81/89/c5818946-3a5b-1af0-fff0-0176d2703f00/AppIcon-0-0-1x_U007emarketing-0-7-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/1347780764_us.js
+++ Library/Caches/com.nan.cac/1347780764_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.netease.idv",
   "id": 1347780764,
   "name": "Identity V",
-  "version": "24.286.1039",
+  "version": "24.301.989",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/10/64/b4/1064b47e-90f0-344f-b10c-5f30f54f1baa/AppIcon-0-0-1x_U007emarketing-0-7-0-85-220.png/100x100bb.jpg"
 }
