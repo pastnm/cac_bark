e4415b72fe4d40cce0ed15a91acc5d53b9e2cf97
GitHub Action <action@github.com>
2024-11-07 06:51:39 +0000

    save caches

--- Library/Caches/com.nan.cac/1403753865_cn.js
+++ Library/Caches/com.nan.cac/1403753865_cn.js
@@ -5,5 +5,5 @@
   "name": "Bark - 给你的手机发推送",
   "version": "1.4.1",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/d1/49/40/d1494096-d36f-3469-8f71-df1c4b713111/AppIcon-0-0-1x_U007epad-0-0-0-1-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/73/f1/e5/73f1e5c9-1d7b-56de-1534-51c63e245ba8/AppIcon-0-0-1x_U007epad-0-0-0-1-0-0-85-220.png/100x100bb.jpg"
 }
