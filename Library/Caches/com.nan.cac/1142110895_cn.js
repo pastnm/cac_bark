4efc5c3629d1872eb2273e55cdc5a2c086c31bd3
GitHub Action <action@github.com>
2024-11-22 19:24:22 +0000

    save caches

--- Library/Caches/com.nan.cac/1142110895_cn.js
+++ Library/Caches/com.nan.cac/1142110895_cn.js
@@ -5,5 +5,5 @@
   "name": "抖音",
   "version": "32.2.0",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/20/e5/50/20e55013-e48b-6af5-e62d-a41f6b23558c/AppIcon-0-0-1x_U007emarketing-0-6-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/29/68/90/29689042-25fd-5a3f-faf6-ffe904ac07f5/AppIcon-0-0-1x_U007emarketing-0-6-0-0-85-220.png/100x100bb.jpg"
 }
