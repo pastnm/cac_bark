4f5ebe918dde60b1ec8024b05a8451e8f431c922
GitHub Action <action@github.com>
2024-12-10 03:11:39 +0000

    save caches

--- Library/Caches/com.nan.cac/1142110895_cn.js
+++ Library/Caches/com.nan.cac/1142110895_cn.js
@@ -5,5 +5,5 @@
   "name": "抖音",
   "version": "32.4.0",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/11/ed/5d/11ed5da8-5f8f-a19f-9722-eddf29333556/AppIcon-0-0-1x_U007emarketing-0-6-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/d1/c1/fd/d1c1fd04-c2a2-5417-c938-07c6ba1eb34b/AppIcon-0-0-1x_U007emarketing-0-6-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/686449807_us.js
+++ Library/Caches/com.nan.cac/686449807_us.js
@@ -3,7 +3,7 @@
   "bundle": "ph.telegra.Telegraph",
   "id": 686449807,
   "name": "Telegram Messenger",
-  "version": 11.5,
+  "version": "11.5.1",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/d2/29/c8/d229c8db-8cc8-bbdf-a460-c4c36e537b46/AppIconLLC-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
 }
