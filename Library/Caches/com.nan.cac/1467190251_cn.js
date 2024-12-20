6b187bb2c1bcdaeb0ea3d5356057448b44a98fe4
GitHub Action <action@github.com>
2024-12-20 02:58:19 +0000

    save caches

--- Library/Caches/com.nan.cac/1467190251_cn.js
+++ Library/Caches/com.nan.cac/1467190251_cn.js
@@ -4,6 +4,6 @@
   "id": 1467190251,
   "name": "原神",
   "version": "5.2.0",
-  "price": null,
+  "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/da/59/da/da59dac4-f9df-5af4-0203-f7759e83e6ef/AppIcon-1x_U007epad-0-0-85-220-0.png/100x100bb.jpg"
 }
