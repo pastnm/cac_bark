b43e5a44846fb2e44219c5eae72aaed1a149cdd8
GitHub Action <action@github.com>
2025-01-01 13:04:35 +0000

    save caches

--- Library/Caches/com.nan.cac/1261944766_cn.js
+++ Library/Caches/com.nan.cac/1261944766_cn.js
@@ -2,8 +2,8 @@
   "status": 0,
   "bundle": "com.ld.TakeBrowser",
   "id": 1261944766,
-  "name": "Alook浏览器 - 8倍速",
+  "name": "Alook浏览器 - 5倍速",
   "version": 20.6,
   "price": "¥6.00",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/a7/fe/10/a7fe107b-06d4-e9ff-ec44-65103f5f5b35/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/a8/15/2d/a8152d02-86dc-30a8-f5af-f219ea21e802/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
 }
--- Library/Caches/com.nan.cac/1467190251_cn.js
+++ Library/Caches/com.nan.cac/1467190251_cn.js
@@ -5,5 +5,5 @@
   "name": "原神",
   "version": "5.3.0",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/8a/0e/2d/8a0e2d44-17a7-1379-8293-a6f9297bfe2b/AppIcon-1x_U007epad-0-0-85-220-0.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/da/59/da/da59dac4-f9df-5af4-0203-f7759e83e6ef/AppIcon-1x_U007epad-0-0-85-220-0.png/100x100bb.jpg"
 }
