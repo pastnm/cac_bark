b8bdd0d7680f1aac07fbcb15dfba659f17767258
GitHub Action <action@github.com>
2025-01-07 23:49:37 +0000

    save caches

--- Library/Caches/com.nan.cac/1261944766_cn.js
+++ Library/Caches/com.nan.cac/1261944766_cn.js
@@ -5,5 +5,5 @@
   "name": "Alook浏览器 - 5倍速",
   "version": 20.7,
   "price": "¥6.00",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/a8/15/2d/a8152d02-86dc-30a8-f5af-f219ea21e802/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/f0/bc/2d/f0bc2d22-c739-e1cc-7dff-0225353a4ecb/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
 }
