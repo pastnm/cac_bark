c99e0f9cf55878830c42adbe840d79c12d822e76
GitHub Action <action@github.com>
2025-01-20 09:30:59 +0000

    save caches

--- Library/Caches/com.nan.cac/1261944766_cn.js
+++ Library/Caches/com.nan.cac/1261944766_cn.js
@@ -3,7 +3,7 @@
   "bundle": "com.ld.TakeBrowser",
   "id": 1261944766,
   "name": "Alook浏览器 - 8倍速",
-  "version": 20.8,
+  "version": 20.9,
   "price": "¥6.00",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/78/33/a1/7833a15d-37cf-8fb8-5ced-94f3cfd698ac/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/100x100bb.jpg"
 }
