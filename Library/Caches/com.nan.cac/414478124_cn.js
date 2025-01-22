480b5fa0dbfbab428b78a01aed9dee2df81dc06c
GitHub Action <action@github.com>
2025-01-22 03:05:13 +0000

    save caches

--- Library/Caches/com.nan.cac/414478124_cn.js
+++ Library/Caches/com.nan.cac/414478124_cn.js
@@ -5,5 +5,5 @@
   "name": "微信",
   "version": "8.0.56",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/f7/25/9c/f7259cf3-41b2-61e5-3655-80b2cc1ae061/AppIcon-0-0-1x_U007emarketing-0-4-0-sRGB-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/1c/50/81/1c5081fa-2458-d46e-659e-992cdc230af1/AppIcon-0-0-1x_U007emarketing-0-4-0-sRGB-0-85-220.png/100x100bb.jpg"
 }
