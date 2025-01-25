65a6eb768c6d53104b68c5e3ce6592c53a92cf59
GitHub Action <action@github.com>
2025-01-25 02:50:39 +0000

    save caches

--- Library/Caches/com.nan.cac/1142110895_cn.js
+++ Library/Caches/com.nan.cac/1142110895_cn.js
@@ -5,5 +5,5 @@
   "name": "抖音 - 欢笑中国年",
   "version": "32.9.0",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/84/21/71/84217160-f405-aec7-04f2-4d8c8cf8b72c/AppIcon-0-0-1x_U007emarketing-0-6-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/57/0b/7c/570b7c1a-e3ac-6f2c-ade4-1841b4640da3/AppIcon-0-0-1x_U007emarketing-0-6-0-0-85-220.png/100x100bb.jpg"
 }
