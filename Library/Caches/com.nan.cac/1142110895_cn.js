a93087f8191d30a4e7f163ec4716229b5eb77862
GitHub Action <action@github.com>
2025-02-05 17:27:33 +0000

    save caches

--- Library/Caches/com.nan.cac/1142110895_cn.js
+++ Library/Caches/com.nan.cac/1142110895_cn.js
@@ -2,8 +2,8 @@
   "status": 0,
   "bundle": "com.ss.iphone.ugc.Aweme",
   "id": 1142110895,
-  "name": "抖音 - 欢笑中国年",
+  "name": "抖音",
   "version": "33.0.0",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/57/0b/7c/570b7c1a-e3ac-6f2c-ade4-1841b4640da3/AppIcon-0-0-1x_U007emarketing-0-6-0-0-85-220.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/3e/19/03/3e1903c4-0575-304d-5c30-c1b01bc3a65c/AppIcon-0-0-1x_U007emarketing-0-6-0-0-85-220.png/100x100bb.jpg"
 }
