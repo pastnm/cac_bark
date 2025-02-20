5c9f3a408d61817f7aa3d390988d49a077e4a6d1
GitHub Action <action@github.com>
2025-02-20 13:35:10 +0000

    save caches

--- Library/Caches/com.nan.cac/444934666_cn.js
+++ Library/Caches/com.nan.cac/444934666_cn.js
@@ -5,5 +5,5 @@
   "name": "QQ",
   "version": "9.1.50",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/20/7b/28/207b28a6-c836-43a2-f009-cdf5c607648b/AppIcon-1-0-1x_U007epad-0-1-0-sRGB-85-220-0.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/fe/80/37/fe80378f-dfae-edcc-2d40-643181544075/AppIcon-1-0-1x_U007epad-0-1-0-sRGB-85-220-0.png/100x100bb.jpg"
 }
