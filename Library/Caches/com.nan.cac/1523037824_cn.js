0f90eecfc89bbbc70f91274a6de51a5782083f54
GitHub Action <action@github.com>
2025-02-27 03:01:36 +0000

    save caches

--- Library/Caches/com.nan.cac/1523037824_cn.js
+++ Library/Caches/com.nan.cac/1523037824_cn.js
@@ -5,5 +5,5 @@
   "name": "崩坏：星穹铁道",
   "version": "3.1.0",
   "price": "免费",
-  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/c2/15/cc/c215ccc3-4335-cb96-06d2-5b4576a54bb4/AppIcon-1x_U007emarketing-0-7-0-85-220-0.png/100x100bb.jpg"
+  "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/f5/f6/5a/f5f65a98-e0d3-286d-8dac-5d52700b5a31/AppIcon-1x_U007emarketing-0-7-0-85-220-0.png/100x100bb.jpg"
 }
