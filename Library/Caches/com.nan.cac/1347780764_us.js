b3a2010693aa19cdff385301a8c6b2d7f60bd342
GitHub Action <action@github.com>
2025-03-01 04:50:27 +0000

    save caches

--- Library/Caches/com.nan.cac/1347780764_us.js
+++ Library/Caches/com.nan.cac/1347780764_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.netease.idv",
   "id": 1347780764,
   "name": "Identity V",
-  "version": "25.57.817",
+  "version": "25.59.2000",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/fe/06/81/fe068198-a44d-4343-a3bc-de2d23930f3d/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/100x100bb.jpg"
 }
