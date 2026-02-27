c6a9c419cfb7d803864a3d987750d3557b0a3c1a
Youth <yakdpa@gmail.com>
2026-02-27 10:45:38 +0800

    更新 1142110895_cn.js

--- Library/Caches/com.nan.cac/1142110895_cn.js
+++ Library/Caches/com.nan.cac/1142110895_cn.js
@@ -3,7 +3,7 @@
   "id": 1142110895,
   "bundle": "com.ss.iphone.ugc.Aweme",
   "name": "抖音 - 欢笑中国年",
-  "version": "37.7.0",
+  "version": "37.6.0",
   "version_id": 882076588,
   "version_release_timestamp": "2026-02-05T09:08:20Z",
   "price": null,
