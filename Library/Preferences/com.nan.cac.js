ac5ec1e6a034e4005669aea988d0d8e52daf59f6
Youth <yakdpa@gmail.com>
2026-03-08 09:39:57 +0800

    更新 com.nan.cac.js

--- Library/Preferences/com.nan.cac.js
+++ Library/Preferences/com.nan.cac.js
@@ -64,18 +64,17 @@
       "name": "元梦之星",
       "url": "https://apps.apple.com/cn/app/id1632344522"
     },
-            {
+                {
       "enable": 1,
       "name": "维克音乐",
       "url": "https://apps.apple.com/cn/app/id6740429002",
       "bark": {
         "body": {
-          "cache": "当前最新版本：app_version\\n当前价格：app_price\\n已缓存到本地等待下一次对比",
-          "up": "发现变更：\\n版本：app_version\\n价格：app_price"
+          "cache": "当前版本: app_version\\n当前价格: app_price",
+          "up": "新版: app_version | 价格: app_price"
         }
       }
     },
-    {
       "enable": 1,
       "name": "QQ 音乐",
       "url": "https://apps.apple.com/cn/app/id414603431",
