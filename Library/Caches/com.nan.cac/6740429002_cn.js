8f717cacd5c05e5ca3d364a52ba17c5eb61a1095
Youth <yakdpa@gmail.com>
2026-03-08 09:00:25 +0800

    更新 6740429002_cn.js

--- Library/Caches/com.nan.cac/6740429002_cn.js
+++ Library/Caches/com.nan.cac/6740429002_cn.js
@@ -6,6 +6,6 @@
   "version": "3.3.6",
   "version_id": 882595825,
   "version_release_timestamp": "2026-02-24T01:55:23Z",
-  "price": "CNY 68.00",
+  "price": "CNY 30.00",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/36/a2/2a/36a22a7a-f117-dd2b-24e8-8286f584747f/AppIcon_Pro-0-0-1x_U007epad-0-1-0-85-220.png/100x100bb.jpg"
 }
